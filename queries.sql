62881e285858d7c967b96406a3d167909cf28569
Eric Worth <Eric Worth>
2017-02-14 17:44:16 -0500

    Add SQL

---
+++ queries.sql
@@ -0,0 +1,22 @@
+SELECT column_name(s)
+FROM table1
+JOIN table2
+ON table1.column_name=table2.column_name;
+
+
+
+
+
+SELECT column_name(s)
+FROM table1
+RIGHT OUTER JOIN table2
+ON table1.column_name=table2.column_name;
+
+
+
+
+
+SELECT column_name(s)
+FROM table1
+FULL OUTER JOIN table2
+ON table1.column_name=table2.column_name;
